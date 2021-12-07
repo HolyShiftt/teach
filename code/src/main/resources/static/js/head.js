d67ac3e97cda85f53b35017fedd8b9d28b2b669e
HolyShiftt <1148088067@qq.com>
2021-12-07 11:33:29 +0800

    导航栏根据角色进行添加菜单

--- code/src/main/resources/static/js/head.js
+++ code/src/main/resources/static/js/head.js
@@ -5,10 +5,23 @@ layui.use(['layer', 'element'], function () {
     // 右上角显示用户名
     $("#realName").text(sessionStorage.getItem("realName"))
 
+    $("#headBar").append("<li class='layui-nav-item layui-show-xs-inline-block layui-hide-sm' lay-header-event='menuLeft'> <i class='layui-icon layui-icon-spread-left\'></i></li>")
+
     // 根据角色判断添加的菜单
-    if (sessionStorage.getItem("role") == 1){
-        $("#headBar").append("<li class='layui-nav-item layui-hide-xs' id='user'><a href='user'>用户管理</a></li>")
+    if (sessionStorage.getItem("role") == 1) {
+        $("#headBar").append("<li class='layui-nav-item layui-hide-xs' id='personal'><a href='personal'>个人信息</a></li>" +
+            "<li class='layui-nav-item layui-hide-xs' id='work'><a href='work'>我的题库</a></li>" +
+            "<li class='layui-nav-item'><a href='javascript:;'>我的考试</a>" +
+            "<dl class='layui-nav-child'>" +
+            "<dd><a href=''>考试安排</a></dd>" +
+            "<dd><a href=''>历史考试</a></dd>" +
+            "</dl>" +
+            "</li><li class='layui-nav-item layui-hide-xs' id='user'><a href='user'>用户管理</a></li>");
+    }else if (sessionStorage.getItem("role") == 2) {
+        $("#headBar").append("<li class='layui-nav-item layui-hide-xs' id='personal'><a href='personal'>个人信息</a></li>" +
+            "</li><li class='layui-nav-item layui-hide-xs' id='user'><a href='user'>用户管理</a></li>");
     }
+
 
     // 注销
     $("#out").click(function () {
