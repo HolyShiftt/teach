2f3d19ad9bd9a1f96413b025fa0e7f14a2ddb8a1
LiuHongYu <1148088067@qq.com>
2022-04-15 11:55:35 +0800

    bug

--- code/src/main/resources/static/js/exam/examStudent.js
+++ code/src/main/resources/static/js/exam/examStudent.js
@@ -11,14 +11,13 @@ layui.use(['layer', 'form'], function () {
         + '&stuId=' + sessionStorage.getItem("stuId"), function (d) {
         for (const exam of d.data) {
             var btn;
-            sessionStorage.setItem("endTime", exam.endTime);
             if (new Date(exam.endTime) >= new Date()) {
                 $("#examTable").append(`<div class="layui-card" style="margin-top: 20px">
                 <div class="layui-card-header" style="padding:10px"><h1>` + exam.name + `</h1></div>
                 <div class="layui-card-body">
                 时间:` + exam.startTime + `  ——  ` + exam.endTime + `<br><br>
                 <h2>科目:` + exam.subject + `</h2><br><br>
-                <button class="layui-btn" onclick="toExam(` + exam.id + `,'` + exam.startTime + `')">进入考试</button>
+                <button class="layui-btn" onclick="toExam(` + exam.id + `,'` + exam.startTime + `','` + exam.endTime + `')">进入考试</button>
                 </div>
             </div>`)
             }
@@ -26,7 +25,8 @@ layui.use(['layer', 'form'], function () {
         }
     })
 
-    window.toExam = function (id, startTime) {
+    window.toExam = function (id, startTime,endTime) {
+        sessionStorage.setItem("endTime", endTime);
         var myDate = new Date();
         var now = myDate.valueOf();
         var time = new Date(startTime).valueOf();
